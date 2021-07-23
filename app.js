a8b92cef9f97c2b525a14b2a5df1a0df3e52df4f
Mark Swan <ms.freemind@gmail.com>
2021-07-22 20:49:47 -0600

    Added setup for MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const feedings = require("./routes/api/feedings");
 
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/api/feedings", feedings);
 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://admin:ICqcCIa4aFMBpmqJ@cluster0.7c8ca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
